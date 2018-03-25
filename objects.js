bc5e6f9b7ca64f6fb0fc703cccd06b4c1f4114ad
LOI DINH <loiphatdinh@gmail.com>
2018-03-25 01:21:17 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(object, key, value){
   object[key] = value;
   return object;
 }
+
+function removeFromPlaylist(playlist, artistName) {
+  delete object.artistName;
+  return object;
+}
